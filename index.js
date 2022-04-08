72b6b014cd0aa1ca120be82c2dec3eeef8000393
aweiler4 <102678755+aweiler4@users.noreply.github.com>
2022-04-08 09:42:09 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let firstName = "Alex";
+let lastName = "Weiler";
+console.log(lastName + " " + firstName);
+
+let enoughMoney = 35;
+let yourMoney = 12;
+if (yourMoney<enoughMoney){
+    console.log("Sorry, you don't have enough money for the concert")
+} else {console.log("You can go to the concert!")
+}
+
+let x = 4;
+let y = 7;
+let z = x + y;
+let a = 23;
+let b = 71;
+let c = a +y;
+
+console.log((x + y + z + a + b + c) / 6)
+
+window.alert('Hey now')
